9bb9eeee6d7fdbbbd7c4b794e3565ce84a0e7f0a
leslie-huang <lesliehuang@gmail.com>
2017-03-31 16:46:47 -0400

    Added new functions for generating binary states from sentiment

---
+++ R/generate_lead_state.R
@@ -0,0 +1,22 @@
+#' Function to create a new lead variable from an existing variable
+#'
+#'
+#' @param df Dataframe containing sentiment levels
+#' @param date Name of var that contains date var for sorting
+#' @param existing_state Variable in df containing current data (at time = t)
+#' @param lead_state Name for variable in df to contain data for time = t+1
+#'
+#' @keywords lag, lead, data
+#' @export
+#' @examples generate_lead_state(df, state_t, state_t_lead)
+
+generate_lead_state <- function(df, date, existing_state, lead_state) {
+
+  df[lead_state] <- NA
+
+  for (i in 1:length(df[, 1]) -1) {
+    df[lead_state][i, ] <- df[existing_state][i+1, ]
+  }
+
+  return(df)
+}
---
+++ R/generate_states.R
@@ -0,0 +1,56 @@
+#' Function to coarsen sentiment data into binary states
+#'
+#'
+#' @param df Dataframe containing sentiment levels
+#' @param date Name of var that contains date var for sorting
+#' @param side Name of var in df that contains side
+#' @param side1 Name to assign side 1
+#' @param side2 Name to assign side 2
+#' @param state_var Name to assign the state variable
+#' @param sentiment_var Name of the var in df that contains sentiment level
+#'
+#' @keywords coarsen, sentiment
+#' @export
+#' @examples generate_states(mnl_df, date side, FARC, govt, state_t, sentiment_level)
+
+
+generate_states <- function(df, date, side_var, side1, side2, state_var, sentiment_var) {
+
+  # first sort the data by date -- OK if it's not regular time series!
+
+  df <- df[order(as.Date(df$date, format = "%Y-%m-%d")), ]
+
+  df[state_var] <- NA
+
+  # fill in the appropriate states
+  for (i in 1:length(df[, 1])) {
+
+    # Divide side1 into states 1 and 2
+    if (df[side_var][i, ] == side1) {
+
+      if (df[sentiment_var][i, ] == 0) {
+        df[state_var][i, ] <- 1
+      }
+
+      else {
+        df[state_var][i, ] <- 2
+      }
+    }
+
+    # Divide side2 into states 3 and 4
+    if (df[side_var][i, ] == side2) {
+
+      if (df[sentiment_var][i, ] == 0) {
+        df[state_var][i, ] <- 3
+      }
+
+      else {
+        df[state_var][i, ] <- 4
+      }
+
+    }
+  }
+
+
+  return(df)
+}
