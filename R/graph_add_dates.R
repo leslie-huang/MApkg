166d35cae2d01872220bbb8950396c73f734423e
leslie-huang <lesliehuang@gmail.com>
2017-04-02 18:30:52 -0400

    Added new function to add events to graphs

---
+++ R/graph_add_dates.R
@@ -0,0 +1,23 @@
+#' Function to add major dates to a base graph
+#'
+#' This function adds major agreements to a base graph
+#' @param base_graph
+#' @param title Title for the graph
+#' @param data Dataframe of dates with categorical labels
+#' @param group Group i.e. categorical variable value to graph
+#' @param legend_label Name of group for the graph legend
+#'
+#' @keywords ggplot, graph
+#' @export
+#' @examples graph_add_dates(negative_sentiment_graph, "Negative Sentiment and Major Agreements", dates, "major_agree", "Major agreements")
+
+graph_add_dates <- function(base_graph, title, data, group_label, legend_label){
+  filtered_data <- data[data["group"] == group_label, ]
+  filtered_data["group"] <- legend_label
+
+  graph = base_graph +
+    ggtitle(title) +
+    geom_vline(data = filtered_data, mapping = aes(xintercept = as.numeric(date), color = group), linetype = 2)
+
+  return(graph)
+}
